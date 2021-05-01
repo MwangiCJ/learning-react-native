0c911d04947a21239df315882cce7c0cfff28518
mwangicj <cmwangi@softcom.co.ke>
2021-05-01 13:30:27 +0300

    flexWrap and alignContent

--- App.js
+++ App.js
@@ -12,20 +12,25 @@ export default function App() {
         flexDirection: "row" /**row-reverse, column, column-reverse */,
         justifyContent:
           "center" /**flex-end, flex-start, space-around, space-even, space-between */,
-        alignItems: "center" /**baseline , flex-end, flex-start, stretch, */
+        alignItems: "center" /**baseline , flex-end, flex-start, stretch, */,
+        alignContent: "center", //allignContent only works with wrap
+        flexWrap: "wrap"
       }}
     >
       <View
         style={{
           backgroundColor: "dodgerblue",
           width: 100,
-          height: 300,
-          alignSelf: "flex-start"
+          height: 300
         }}
       ></View>
-      <View style={{ backgroundColor: "gold", width: 100, height: 200 }}></View>
+      <View style={{ backgroundColor: "gold", width: 100, height: 100 }}></View>
       <View
         style={{ backgroundColor: "tomato", width: 100, height: 100 }}
+      ></View>
+      <View style={{ backgroundColor: "grey", width: 100, height: 100 }}></View>
+      <View
+        style={{ backgroundColor: "greenyellow", width: 100, height: 100 }}
       ></View>
     </View>
   );
