268adabb919cd3c12a80acfa937200922091da76
mwangicj <cmwangi@softcom.co.ke>
2021-05-01 10:42:09 +0300

    progress

--- App.js
+++ App.js
@@ -6,18 +6,25 @@ import {
   View,
   SafeAreaView,
   Image,
-  TouchableWithoutFeedback
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  Button,
+  Alert
 } from "react-native";
 
 export default function App() {
   const handlePress = () => console.log("Pressed");
   return (
     <SafeAreaView style={styles.container}>
+      <StatusBar style="auto" />
       <Text numberOfLines={1} onPress={handlePress}>
         Hi jc, Open up App.js to start working on your app!
       </Text>
       <Image source={require("./assets/favicon.png")} />
-      <TouchableWithoutFeedback>
+      <TouchableHighlight
+        onPress={() => console.log("Image tapped")}
+        onLongPress={() => console.log("Image lognpressed")}
+      >
         <Image
           blurRadius={10}
           fadeDuration={1000}
@@ -27,13 +34,31 @@ export default function App() {
             height: 300
           }}
         />
-      </TouchableWithoutFeedback>
-      <StatusBar style="auto" />
+      </TouchableHighlight>
+      <TouchableNativeFeedback>
+        <View
+          style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}
+        ></View>
+      </TouchableNativeFeedback>
+      <Button
+        title="Click Me"
+        onPress={
+          () =>
+            Alert.alert("My Custom Title", "Button Clicked", [
+              { text: "Yes", onPress: () => console.log("Yes Clicked") },
+              { text: "No", onPress: () => console.log("No Clicked") }
+            ])
+          /*Prompt Works only on iOS */
+          //Alert.prompt("Title", "Message", text => console.log(text))
+        }
+        color="orange"
+      />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  /**It has benefits such as validation */
   container: {
     flex: 1,
     backgroundColor: "#fff",
