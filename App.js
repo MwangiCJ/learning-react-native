278d0cb5216e3d55cdf059911fc8696fb6b679ed
mwangicj <cmwangi@softcom.co.ke>
2021-05-01 09:47:08 +0300

    adding images

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Pressed");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hi jc, Open up App.js to start working on your app!
+      </Text>
+      <Image source={require("./assets/favicon.png")} />
+      <TouchableWithoutFeedback>
+        <Image
+          blurRadius={10}
+          fadeDuration={1000}
+          source={{
+            uri: "https://picsum.photos/id/1/200/300",
+            width: 200,
+            height: 300
+          }}
+        />
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    paddingTop: 20,
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
