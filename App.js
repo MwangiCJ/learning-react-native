c2eeac04d84decdd8067e2419d0b8971ec7c19cc
mwangicj <cmwangi@softcom.co.ke>
2021-05-01 13:24:47 +0300

    justifyContent, alignItems and alignSelf

--- App.js
+++ App.js
@@ -6,18 +6,24 @@ import { View } from "react-native";
 export default function App() {
   return (
     <View
-      style={
-        {
-          backgroundColor: "#fff",
-          flex: 1,
-          flexDirection: "row"
-        } /**row-reverse, column, column-reverse */
-      }
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row" /**row-reverse, column, column-reverse */,
+        justifyContent:
+          "center" /**flex-end, flex-start, space-around, space-even, space-between */,
+        alignItems: "center" /**baseline , flex-end, flex-start, stretch, */
+      }}
     >
       <View
-        style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 300,
+          alignSelf: "flex-start"
+        }}
       ></View>
-      <View style={{ backgroundColor: "gold", width: 100, height: 100 }}></View>
+      <View style={{ backgroundColor: "gold", width: 100, height: 200 }}></View>
       <View
         style={{ backgroundColor: "tomato", width: 100, height: 100 }}
       ></View>
