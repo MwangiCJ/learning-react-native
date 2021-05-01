f49061bd579999cdd2505f3d16ca8c8e24e7e3e8
mwangicj <cmwangi@softcom.co.ke>
2021-05-01 13:47:38 +0300

    flexBasis, flexGrow and flexShrink

--- App.js
+++ App.js
@@ -12,25 +12,24 @@ export default function App() {
         flexDirection: "row" /**row-reverse, column, column-reverse */,
         justifyContent:
           "center" /**flex-end, flex-start, space-around, space-even, space-between */,
-        alignItems: "center" /**baseline , flex-end, flex-start, stretch, */,
-        alignContent: "center", //allignContent only works with wrap
-        flexWrap: "wrap"
+        alignItems: "center" /**baseline , flex-end, flex-start, stretch, */
+        // alignContent: "center", //allignContent only works with wrap
+        //flexWrap: "wrap"
       }}
     >
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width: 100,
-          height: 300
+          //flexBasis: 100, //Width or height,
+          //flexGrow: 1, //same as flex
+          width: 400,
+          flexShrink: 1, //same as flex with negative value
+          height: 100
         }}
       ></View>
       <View style={{ backgroundColor: "gold", width: 100, height: 100 }}></View>
       <View
         style={{ backgroundColor: "tomato", width: 100, height: 100 }}
-      ></View>
-      <View style={{ backgroundColor: "grey", width: 100, height: 100 }}></View>
-      <View
-        style={{ backgroundColor: "greenyellow", width: 100, height: 100 }}
       ></View>
     </View>
   );
