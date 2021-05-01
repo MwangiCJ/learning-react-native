052eca9230c029dcd0f86b849b243d6b2ca0c3f8
mwangicj <cmwangi@softcom.co.ke>
2021-05-01 14:43:42 +0300

    added welcome screen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 function WelcomeScreen(props) {
     return (
-        <ImageBackground style={styles.background} source={require("../assets/background.jpg")}></ImageBackground>
+        <ImageBackground style={styles.background} source={require("../assets/background.jpg")}>
+            <View style={styles.logoContainer}>
+                <Image style={styles.logo} source={require("../assets/logo-red.png")}/>
+                <Text>Sell What you dont need</Text>
+            </View>
+            <View style={styles.loginButton}></View>
+            <View style={styles.registerButton}></View>
+
+        </ImageBackground>
             
    
     );
@@ -11,7 +19,31 @@ function WelcomeScreen(props) {
 
 const styles = StyleSheet.create({
     background:{
-        flex:1
+        flex:1,
+        justifyContent:"flex-end",
+        alignItems:"center"
+    },
+    logoContainer:{
+        position:"absolute",
+        top:70,
+        alignItems: "center"
+    },
+
+    logo:{
+        width:100,
+        height:100,
+        
+        
+    },
+    loginButton:{
+        width:'100%',
+        height:70,
+        backgroundColor:"#fc5c65"
+    },
+    registerButton:{
+        width:'100%',
+        height:70,
+        backgroundColor:"#4ecdc4"
     }
 })
 
