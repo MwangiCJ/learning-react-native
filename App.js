7d8d5ca9dce4f83a8adbcd5876f763f0f855555e
mwangicj <cmwangi@softcom.co.ke>
2021-05-16 12:38:07 +0300

    working with expo @expo/vector-icons

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { View, Text } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import { View } from "react-native";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import AppText from "./components/AppText";
 
 export default function App() {
   // return <WelcomeScreen/>;
-  return <ViewImageScreen />;
+  //return <ViewImageScreen />;
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <AppText>Hello, Jc</AppText>
+      <MaterialCommunityIcons name="email" size={200} color="gold" />
+    </View>
+  );
 }
