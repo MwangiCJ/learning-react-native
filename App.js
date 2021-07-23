eccd2c1474a5500170b82b711e44e296512b2dfc
mwangicj <cmwangi@softcom.co.ke>
2021-07-23 06:29:21 +0300

    reusable AppTextInput component

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { View, Text, TextInput } from "react-native";
+import AppTextInput from "./app/components/AppTextInput";
 
 import Screen from "./app/screens/Screen";
 
@@ -8,17 +9,7 @@ export default function App() {
 
   return (
     <Screen>
-      <View>
-        <Text>{firstName}</Text>
-        <TextInput
-          onChangeText={(text) => setFirstName(text)}
-          placeholder="First Name"
-          style={{
-            borderBottomColor: "#ccc",
-            borderBottomWidth: 1,
-          }}
-        />
-      </View>
+      <AppTextInput placeholder="Username" icon="email" />
     </Screen>
   );
 }
