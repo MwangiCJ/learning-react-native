09626095df48c96da0f88ee7a514e6b3c535de9a
mwangicj <cmwangi@softcom.co.ke>
2021-07-23 06:06:44 +0300

    started working with input components

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { View, Text } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import AppText from "./app/components/AppText";
-import AppButton from "./app/components/AppButton";
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import React, { useState } from "react";
+import { View, Text, TextInput } from "react-native";
+
 import Screen from "./app/screens/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingScreen from "./app/screens/ListingScreen";
+
 export default function App() {
-  return <ListingScreen />;
+  const [firstName, setFirstName] = useState("");
+
+  return (
+    <Screen>
+      <View>
+        <Text>{firstName}</Text>
+        <TextInput
+          onChangeText={(text) => setFirstName(text)}
+          placeholder="First Name"
+          style={{
+            borderBottomColor: "#ccc",
+            borderBottomWidth: 1,
+          }}
+        />
+      </View>
+    </Screen>
+  );
 }
